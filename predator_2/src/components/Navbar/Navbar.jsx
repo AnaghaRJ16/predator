a98631017b94b691c8c8b8c1ce79fa85383ad103
Anagha R Jadhav <144093247+AnaghaRJ16@users.noreply.github.com>
2025-01-26 01:50:55 +0530

    .

---
+++ predator_2/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import "./Navbar.css";
+import logo from "../../assets/logo.png"; // Updated to use logo.png
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#weather">Weather Forecasting</a>
+    </p>
+    <p>
+      <a href="#chatbot">ChatBot</a>
+    </p>
+    <p>
+      <a href="#aboutus">About Us</a>
+    </p>
+    <p>
+      <a href="#contactus">Contact Us</a>
+    </p>
+  </>
+);
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" /> 
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container">
+            <Menu />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
