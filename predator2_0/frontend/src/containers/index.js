b736befbc5f073dff895c7b8390c2c7e9b0fead0
Pooja Goudar <166782091+PoojaGoudar@users.noreply.github.com>
2025-01-26 11:52:52 +0530

    /

--- predator2_0/frontend/src/containers/index.js
+++ predator2_0/frontend/src/containers/index.js
@@ -1,4 +1,4 @@
-export { default as chatBot } from "./chatbot/chatbot";
+export { default as chatBot } from "./Chatbot/chatbot";
 export { default as About } from "./About/About";
 export { default as Contact } from "./contact/contact";
 export { default as weather } from "./weather/weather";
